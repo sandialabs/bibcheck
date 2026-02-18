d250ce3e6b12fa040d3463c6cf9f1c180466107f
Carl Pearson <cwpears@sandia.gov>
2026-02-17 17:17:21 -0700

    Use Llama 3.3 70B for entry extractions
    Signed-off-by: Carl Pearson <cwpears@sandia.gov>

--- lookup/entry.go
+++ lookup/entry.go
@@ -284,6 +284,8 @@ func Entry(text string, mode string,
 		fmt.Println("  Title:  ", online.Title)
 		fmt.Println("  Authors:", strings.Join(online.Authors, ", "))
 
+		// TODO: we can somehow do format=markdown for github, which might produce better results
+
 		if body, contentType, err := retrieveUrl(online.URL); err != nil {
 			EA.Online.Error = fmt.Errorf("retrieve url error: %w", err)
 		} else {
